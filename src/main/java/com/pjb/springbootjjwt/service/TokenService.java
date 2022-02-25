00db3ab4e320bf8514314ef3983e9b571368dab9
001026ab <1176754512@qq.com>
2022-02-25 17:58:42 +0800

    测试分支3，F:\springboot-jwt-master\springboot-jwt文件夹

--- src/main/java/com/pjb/springbootjjwt/service/TokenService.java
+++ src/main/java/com/pjb/springbootjjwt/service/TokenService.java
@@ -1,5 +1,6 @@
 package com.pjb.springbootjjwt.service;
 
+import com.alibaba.fastjson.JSON;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.pjb.springbootjjwt.entity.User;
@@ -13,8 +14,14 @@ import org.springframework.stereotype.Service;
 @Service("TokenService")
 public class TokenService {
     public String getToken(User user) {
-        String token="";
+      /*  String token="";
         token= JWT.create().withAudience(user.getId())// 将 user id 保存到 token 里面
+                .sign(Algorithm.HMAC256(user.getPassword()));// 以 password 作为 token 的密钥
+        return token;*/
+        //整个user对象
+        String s = JSON.toJSON(user).toString();
+        String token="";
+        token= JWT.create().withAudience(s)// 将整个对象/将 user id 保存到 token 里面
                 .sign(Algorithm.HMAC256(user.getPassword()));// 以 password 作为 token 的密钥
         return token;
     }
